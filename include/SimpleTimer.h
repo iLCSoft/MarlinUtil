1e3d10371f3d4ad41d999cd209542640a0697a8f
Oliver Wendt <oliver.wendt@desy.de>
2005-11-04 16:55:29 +0000

    documentation

--- include/SimpleTimer.h
+++ include/SimpleTimer.h
@@ -19,7 +19,22 @@ using namespace marlin ;
 
 
 
+/**
+ *    Simple timimg processor, which offers an delay after an event has been processed.
+ *
+ *    @param SecondsToWait : time to wait (in seconds)
 
+ *    @param Mode : toggle for two different timing modes <br>
+ *                  0 - just wait the time which is specified in the parameter
+ *                      SecondsToWait <br>
+ *                  1 - calculate the elapsed time since the last call of this processor
+ *                      (e.g. in the last event) and wait for the remaining time 
+ *                      (SecondsToWait minus elapsed time). If already more time elapsed 
+ *                      than SecondsToWait, carry on without any delay.
+ *    @author O. Wendt (DESY)
+ *    @version $Id: SimpleTimer.h,v 1.2 2005-11-04 16:55:29 owendt Exp $
+ *
+ */
 class SimpleTimer : public Processor {
   
  public:
