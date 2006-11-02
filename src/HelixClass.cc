af30e80ca4460c8d18ddd908026e20e344ff10f8
Alexei Raspereza <alexei.raspereza@desy.de>
2006-11-02 12:20:24 +0000

    Modified HelixClass

--- src/HelixClass.cc
+++ src/HelixClass.cc
@@ -1,12 +1,9 @@
 
-// FIXME: all methods concerning positions (or more general, concerning return values of LCIO class methods in double precision) need to be overloaded as double precision methods as well (2006/06/19).
-
 #include "HelixClass.h"
 #include <math.h>
 #include <stdlib.h>
 #include <iostream>
 #include "ced_cli.h"
-
 
 HelixClass::HelixClass() {
     _const_pi = acos(-1.0);
@@ -18,8 +15,6 @@ HelixClass::HelixClass() {
 HelixClass::~HelixClass() {}
 
 void HelixClass::Initialize_VP(float * pos, float * mom, float q, float B) {
-
-  // FIXME: the initialisation is not consistent for all the parametrisations
     _referencePoint[0] = pos[0];
     _referencePoint[1] = pos[1];
     _referencePoint[2] = pos[2];
@@ -70,13 +65,6 @@ void HelixClass::Initialize_VP(float * pos, float * mom, float q, float B) {
     }
     _z0 = pos[2] + _radius*_tanLambda*q*(deltaPhi + _const_2pi*nCircles);
 
-
-    // debug
-    /*
-    std::cout << "Initialize_VP called : " << std::endl << std::endl;
-    printParameters();
-    */
-
 }
 
 void HelixClass::Initialize_Canonical(float phi0, float d0, float z0, 
@@ -107,16 +95,6 @@ void HelixClass::Initialize_Canonical(float phi0, float d0, float z0,
     _phiAtPCA = atan2(-_yCentre,-_xCentre);
     _phiRefPoint =  _phiAtPCA ;
     _bField = B;
-
-    _bZ = _omega/_tanLambda;
-
-
-    // debug
-    /*
-    std::cout << "Initialize_Canonical called: " << std::endl << std::endl;
-    printParameters();  
-    */
-
 }
 
 
@@ -124,7 +102,6 @@ void HelixClass::Initialize_BZ(float xCentre, float yCentre, float radius,
 			       float bZ, float phi0, float B, float signPz,
 			       float zBegin) {
 
-  // FIXME: _bZ and phi0 are NOT initialised correctely here and within the other two init methods
   _radius = radius;
   _pxy = _FCT*B*_radius;
   _charge = -(bZ*signPz)/fabs(bZ*signPz);
@@ -176,15 +153,6 @@ void HelixClass::Initialize_BZ(float xCentre, float yCentre, float radius,
   _z0 = _referencePoint[2] - (deltaPhi + _const_2pi*nCircles)/bZ;  
   _bField = B;
 
-  _bZ = bZ;
-
-
-  // debug
-  /*
-  std::cout << "Initialize_BZ called: " << std::endl << std::endl;
-  printParameters();  
-  */
-  
 }
 
 const float * HelixClass::getMomentum() {
@@ -194,7 +162,9 @@ const float * HelixClass::getReferencePoint() {
     return _referencePoint;
 }
 float HelixClass::getPhi0() {
-    return _phi0;
+  if (_phi0<0.0)
+    _phi0 += 2*acos(-1.);
+  return _phi0;
 }
 float HelixClass::getD0() {
     return _d0;
@@ -458,40 +428,7 @@ float HelixClass::getPointInZ(float zLine, float * ref, float * point) {
 
 }
 
-
-
-float HelixClass::getDistanceToPoint(float* xPoint, float* Distance) {
-
-  float pointOnHelix[3] = {0.0,0.0,0.0};
-  
-  return getDistanceToPoint(xPoint,Distance,pointOnHelix);
-
-}
-
-
-float HelixClass::getDistanceToPoint(const float* xPoint, float* Distance) {
-
-  float pointOnHelix[3] = {0.0,0.0,0.0};
-
-  float x[3] = {0.0,0.0,0.0};   
-  for (unsigned int i = 0; i < 3; ++i) x[i] = xPoint[i];
-
-  return getDistanceToPoint(x,Distance,pointOnHelix);
-
-}
-
-
-float HelixClass::getDistanceToPoint(const float* xPoint, float* Distance, float* pointOnHelix) {
-
-  float x[3] = {0.0,0.0,0.0};   
-  for (unsigned int i = 0; i < 3; ++i) x[i] = xPoint[i];
-
-  return getDistanceToPoint(x,Distance,pointOnHelix);
-
-}
-
-
-float HelixClass::getDistanceToPoint(float* xPoint, float* Distance, float* pointOnHelix) {
+float HelixClass::getDistanceToPoint(float * xPoint, float * Distance) {
 
   float xOnHelix, yOnHelix, zOnHelix;
   float phi = atan2(xPoint[1]-_yCentre,xPoint[0]-_xCentre);
@@ -540,14 +477,10 @@ float HelixClass::getDistanceToPoint(float* xPoint, float* Distance, float* poin
   else {
     Time = _charge*_radius*DPhi/_pxy;
   }
-  
+
   Distance[0] = DistXY;
   Distance[1] = DistZ;
   Distance[2] = sqrt(DistXY*DistXY+DistZ*DistZ);
-  
-  pointOnHelix[0] = xOnHelix;
-  pointOnHelix[1] = yOnHelix;
-  pointOnHelix[2] = zOnHelix;
 
   return Time;
 
@@ -555,8 +488,6 @@ float HelixClass::getDistanceToPoint(float* xPoint, float* Distance, float* poin
 }
 
 void HelixClass::setHelixEdges(float * xStart, float * xEnd) {
-
-  // FIXME: This method is not used at all. 2006/06/21 OW
   for (int i=0; i<3; ++i) {
     _xStart[i] = xStart[i];
     _xEnd[i] = xEnd[i];
@@ -575,10 +506,9 @@ float * HelixClass::getDistanceToHelix(HelixClass * helix, float * pos, float * 
   float rad1 = _radius;
   float rad2 = helix->getRadius();
 
-  float distance = sqrt((x01-x02)*(x01-x02)+(y01-y02)*(y01-y02));
+  
 
-  // FIXME: change return value of this method
-  return pos;
+  float distance = sqrt((x01-x02)*(x01-x02)+(y01-y02)*(y01-y02));
 
 }
 
@@ -597,73 +527,4 @@ void HelixClass::getExtrapolatedMomentum(float * pos, float * momentum) {
   momentum[2] = _momentum[2];
 
 
-}
-
-
-float HelixClass::getPathLength(float* point1, float* point2) {
-
-  // FIXME: check mechanism whether the points are on the helix or not is needed
-  
-  float pathLength = sqrt( (_bZ*_radius)*(_bZ*_radius) + 1 ) * (point2[2] - point1[2]);
-
-  // for integration in negativ z direction change the sign:
-  if ( pathLength < 0.0 ) pathLength *= -1.0;
-
-  return pathLength;
-
-}
-
-
-bool HelixClass::isOnHelix(const float* point) {
-
-  // still missing
-
-  return false;
-
-}
-
-
-void HelixClass::printParameters() {
-
-  
-  std::cout << "Printout of the different helix parameters: " << std::endl << std::endl;
-  std::cout << "0. Parametrisation, i.e. x_vec, p_vec, q and B ( done with 'Initialize_VP(float * pos, float * mom, float q, float B)' ): " << std::endl;
-  std::cout << "pos = " << "( " << _referencePoint[0] << ", " << _referencePoint[1] << ", " << _referencePoint[2] << " )" << " [ pos = referencePoint ]" << std::endl 
-	    << "p @ pos = " << "( " << _momentum[0] << ", " << _momentum[1] << ", " << _momentum[2] << " )" << std::endl 
-	    << "q = " << _charge << std::endl
-	    << "B = " << _bField << std::endl << std::endl;
-
-  std::cout << "1. Parametrisation, i.e. xc, yc, r, bZ, phi0, B, sin(pz) and z0 ( done with 'Initialize_BZ(float xCentre, float yCentre, float radius, float bZ, float phi0, float B, float signPz, float zBegin)' ): " << std::endl;
-  std::cout << "xc = " << _xCentre << std::endl
-	    << "yc = " << _yCentre << std::endl
-	    << "r  = " << _radius  << std::endl
-	    << "bZ = " << _bZ      << std::endl
-	    << "phi0 = " << _phi0  << std::endl
-	    << "B = " << _bField   << std::endl
-            << "signPz = " << _momentum[2]/fabs(_momentum[2]) << " ( ??? )" << std::endl
-	    << "z0 = " << _z0 << " ( ??? )" << std::endl << std::endl;
-
-  std::cout << "2. Parametrisation, i.e. xc, yc, zc, R, omega, phi0, v0, is still missing." << std::endl << std::endl;
-
-  std::cout << "3. Parametrisation ( L3 like ), i.e. phi0, d0, z0, Omega, tanlam and  B ( done with 'Initialize_Canonical(float phi0, float d0, float z0, float omega, float tanlambda, float B)' ): " << std::endl;
-  std::cout << "phi0 = " << _phi0  << std::endl
-	    << "d0 = " << _d0 << std::endl
-	    << "z0 = " << _z0 << std::endl
-	    << "Omega = " << _omega << std::endl
-	    << "tanlambda = " << _tanLambda << std::endl
-	    << "B = " <<  _bField << std::endl
-	    << "refPoint = " << "( " << _referencePoint[0] << ", " << _referencePoint[1] << ", " << _referencePoint[2] << " )" << std::endl << std::endl;
-
-  std::cout << "Other member variables:" << std::endl;
-  std::cout << "pxy = " << _pxy << std::endl
-	    << "phiRefPoint = " << _phiRefPoint << std::endl
-	    << "phiAtPCA = " << _phiAtPCA << std::endl
-	    << "xAtPCA = " << _xAtPCA << std::endl
-	    << "yAtPCA = " << _yAtPCA << std::endl
-	    << "pxAtPCA = " << _pxAtPCA << std::endl
-	    << "pyAtPCA = " << _pyAtPCA << std::endl
-	    << "phiMomRefPoint = " << _phiMomRefPoint << std::endl
-	    << "xStart = " << "( " << _xStart[0] << ", " << _xStart[1] << ", " << _xStart[2] << " )" << std::endl 
-	    << "xEnd = " << "( " << _xEnd[0] << ", " << _xEnd[1] << ", " << _xEnd[2] << " )" << std::endl << std::endl;
-    
 }
