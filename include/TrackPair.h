b6caa3b59c9d89b67f200961fb2bb8b683d57db8
Alexei Raspereza <alexei.raspereza@desy.de>
2008-07-08 08:41:57 +0000

    New class used for association of tracks constituting neutral vertices

---
+++ include/TrackPair.h
@@ -0,0 +1,50 @@
+#ifndef TRACKPAIR_H 
+#define TRACKPAIR_H 1
+#include "lcio.h"
+#include "EVENT/LCIO.h"
+#include "EVENT/Track.h"
+#include <vector>
+
+using namespace lcio;
+
+class TrackPair;
+
+typedef std::vector<TrackPair*> TrackPairVec;
+
+class TrackPair {
+
+ public:
+
+  TrackPair( );
+  ~TrackPair( );
+
+  TrackVec & getTracks();
+  Track * getFirstTrack();
+  Track * getSecondTrack();
+  float getDistance();
+  float * getVertex();
+  float * getMomentum();
+  float getMass();
+  int getCode();
+
+  void setFirstTrack( Track * track );
+  void setSecondTrack( Track * track );
+  void setDistance(float distance);
+  void setVertex( float * vertex );
+  void setMomentum( float * momentum );
+  void setMass( float mass);
+  void setCode( int code );
+
+ private:
+
+  TrackVec  _trkVec;
+  float _distance;
+  float * _vertex;
+  float * _momentum;
+  float _mass;
+  int _code;
+
+};
+
+
+#endif
