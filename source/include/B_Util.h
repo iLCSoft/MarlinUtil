6b1bdae2bd70489e160244007c28141d0faa5451
Andre Sailer <andre.philippe.sailer@cern.ch>
2017-03-27 18:10:56 +0200

    B_Util: use initialiser list for ctors

--- source/include/B_Util.h
+++ source/include/B_Util.h
@@ -15,8 +15,8 @@ class Point2D {
   double x;  // Cartesian coordinate 
   double y;  // Cartesian coordinate
 
-  double rz;
   double rz2;
+  double rz;
   
   void calc() { rz2=x*x+y*y; rz=sqrt(rz2); }
   void calc(double _x,double _y) { x=_x; y=_y; rz2=x*x+y*y; rz=sqrt(rz2); }
@@ -24,9 +24,9 @@ class Point2D {
   double dist2(Point2D &a) { return (x-a.x)*(x-a.x)+(y-a.y)*(y-a.y); }
   double dist(Point2D &a) { return sqrt(dist2(a)); }
 
-  Point2D() : x(0.),y(0.),rz(0.),rz2(0.) { }
-  Point2D(double _x,double _y) { calc(_x,_y); }
-  Point2D(Point2D const & a) { calc(a); }
+  Point2D() : x(0.),y(0.),rz2(0.),rz(0.) { }
+  Point2D(double _x,double _y) : x(_x), y(_y), rz2(x*x+y*y), rz(sqrt(rz2)) {}
+  Point2D(Point2D const & a): x(a.x), y(a.y), rz2(a.rz2), rz(a.rz)  {}
   Point2D& operator=(const Point2D &a) { calc(a); return *this; }
 };
 
@@ -54,8 +54,8 @@ class Point3D : public Point2D {
   }
   
   Point3D() : Point2D(),z(0.),r(0.) {}
-  Point3D(double _x,double _y,double _z) : Point2D(_x,_y) { z=_z; r=sqrt(rz2+z*z); }
-  Point3D(Point3D const &a) : Point2D(a) { z=a.z; r=a.r; }
+  Point3D(double _x,double _y,double _z) : Point2D(_x,_y), z(_z), r(rz2+z*z) {}
+  Point3D(Point3D const &a) : Point2D(a), z(a.z), r(a.r) {}
 
   double rdist(Point3D &a) { return fabs(a.r-r); }
 
