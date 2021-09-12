29fa7ac11bedd816fcd685a5a8ba7a280b4c5a4a
Bohdan Dudar <bohdan.dudar@desy.de>
2021-09-13 00:47:57 +0200

    Added getDetData() methods to the GeometryUtils

--- source/include/GeometryUtil.h
+++ source/include/GeometryUtil.h
@@ -26,6 +26,49 @@ namespace MarlinUtil {
   dd4hep::rec::LayeredCalorimeterData const* getLayeredCalorimeterData(unsigned int includeFlag,
                                                                        unsigned int excludeFlag=0);
 
+
+   /**
+   * Returns DDRec detector extension ZPlanarData for vertex detector
+   * (link to the dd4hep documentation of the class so user knows which parameters he get get from this...)
+   */
+   dd4hep::rec::ZPlanarData* getVXDData();
+
+
+   /**
+   * Returns DDRec detector extension ZPlanarData for SIT detector
+   * (link to the dd4hep documentation of the class so user knows which parameters he get get from this...)
+   */
+   dd4hep::rec::ZPlanarData* getSITData();
+
+
+   /**
+   * Returns DDRec detector extension ZDiskPetalsStruct for FTD detector
+   * (link to the dd4hep documentation of the class so user knows which parameters he get get from this...)
+   */
+   dd4hep::rec::ZDiskPetalsData* getFTDData();
+
+
+   /**
+   * Returns DDRec detector extension FixedPadSizeTPCData for TPC detector
+   * (link to the dd4hep documentation of the class so user knows which parameters he get get from this...)
+   */
+   dd4hep::rec::FixedPadSizeTPCData* getTPCData();
+
+
+   /**
+   * Returns DDRec detector extension ZPlanarData for SET detector
+   * (link to the dd4hep documentation of the class so user knows which parameters he get get from this...)
+   */
+   dd4hep::rec::ZPlanarData* getSETData();
+
+
+
+
+
+
+
+
+
 }//namespace MarlinUtil
 
 #endif // GEOMETRYUTIL_H
