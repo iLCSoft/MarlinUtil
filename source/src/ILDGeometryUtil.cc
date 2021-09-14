47c98ca20042885acd3175fa990fc50ee79025cc
Bohdan Dudar <bohdan.dudar@desy.de>
2021-09-14 15:49:20 +0200

    fix namespace

--- source/src/ILDGeometryUtil.cc
+++ source/src/ILDGeometryUtil.cc
@@ -1,26 +1,26 @@
 #include "ILDGeometryUtil.h"
 #include "GeometryUtil.h"
 
-dd4hep::rec::ZPlanarData* MarlinUtil::getVXDData(){
+dd4hep::rec::ZPlanarData* MarlinUtil::ILD::getVXDData(){
     return getDetData<dd4hep::rec::ZPlanarData>("VXD");
 }
 
 
-dd4hep::rec::ZPlanarData* MarlinUtil::getSITData(){
+dd4hep::rec::ZPlanarData* MarlinUtil::ILD::getSITData(){
     return getDetData<dd4hep::rec::ZPlanarData>("SIT");
 }
 
 
-dd4hep::rec::ZDiskPetalsData* MarlinUtil::getFTDData(){
+dd4hep::rec::ZDiskPetalsData* MarlinUtil::ILD::getFTDData(){
     return getDetData<dd4hep::rec::ZDiskPetalsData>("FTD");
 }
 
 
-dd4hep::rec::FixedPadSizeTPCData* MarlinUtil::getTPCData(){
+dd4hep::rec::FixedPadSizeTPCData* MarlinUtil::ILD::getTPCData(){
     return getDetData<dd4hep::rec::FixedPadSizeTPCData>("TPC");
 }
 
 
-dd4hep::rec::ZPlanarData* MarlinUtil::getSETData(){
+dd4hep::rec::ZPlanarData* MarlinUtil::ILD::getSETData(){
     return getDetData<dd4hep::rec::ZPlanarData>("SET");
 }
