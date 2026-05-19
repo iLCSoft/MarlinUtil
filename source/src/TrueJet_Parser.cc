c8a8275a6761f60a2512f8440fdce61884d75fea
Juan Miguel Carceller <jmcarcell@users.noreply.github.com>
2026-05-19 14:10:52 +0200

    Remove the unused delall function

--- source/src/TrueJet_Parser.cc
+++ source/src/TrueJet_Parser.cc
@@ -662,15 +662,3 @@ void TrueJet_Parser::getall( LCEvent * event ) {
     reltrue_tj.reset();
 
 }
-void TrueJet_Parser::delall( ) {
-    relfcn.reset();
-    relicn.reset();
-    relfp.reset();
-    relip.reset();
-    reltjreco.reset();
-    reltjmcp.reset();
-    jets.reset();
-    finalcns.reset();
-    initialcns.reset();
-    reltrue_tj.reset();
-}
